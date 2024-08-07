0b6b7b3b15935153afffb2b7b91dc7ce3b0708d4
Junsu Bok <86839362+JunSu2844@users.noreply.github.com>
2024-08-07 19:49:08 +0900

    [Bronze III] Title: 최소, 최대, Time: 1356 ms, Memory: 103772 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,26 @@
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int[] arr = new int[N];
+        int index = 0;
+
+
+        while(st.hasMoreTokens()) {
+            arr[index] = Integer.parseInt(st.nextToken());
+            index++;
+        }
+
+        Arrays.sort(arr);
+        System.out.print(arr[0]+" "+arr[N-1]);
+        br.close(); //입력스트림 닫는다.
+
+    }
+}
