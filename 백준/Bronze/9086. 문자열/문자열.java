73c721b7241e545110516177ce7836e28c7746b1
Junsu Bok <86839362+JunSu2844@users.noreply.github.com>
2024-08-09 15:21:00 +0900

    [Bronze V] Title: 문자열, Time: 120 ms, Memory: 15888 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,22 @@
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+        String str;
+
+        for(int i=0; i<T; i++) {
+            str = br.readLine();
+            bw.write(str.charAt(0) +""+str.charAt(str.length()-1)+"\n");
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
