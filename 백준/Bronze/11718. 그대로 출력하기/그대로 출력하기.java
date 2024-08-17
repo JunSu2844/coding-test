43e3b968ce82886c915a3578155a587170f105b8
Junsu Bok <86839362+JunSu2844@users.noreply.github.com>
2024-08-17 18:13:19 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 100 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,18 @@
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+
+        while((str = br.readLine()) != null) {
+            System.out.println(str);
+        }
+
+        br.close(); //입력스트림 닫기
+
+    }
+}
