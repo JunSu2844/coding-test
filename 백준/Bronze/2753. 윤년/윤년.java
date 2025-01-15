ee907ba8fcc968ecdf54755f17cdad84aa544cf1
Junsu Bok <86839362+JunSu2844@users.noreply.github.com>
2025-01-15 20:39:37 +0900

    [Bronze V] Title: 윤년, Time: 108 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -1,15 +1,22 @@
-import java.util.Scanner;
 
-class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-       
-        
-        if(a%4==0 && (a%100 != 0 || a%400 == 0) ) {
-            System.out.println(1);
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //입력 스트림
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); //출력 스트림
+        int num = Integer.parseInt(br.readLine());
+
+        if(num%4 == 0) {
+            if(num%100 != 0 || num%400 == 0) {
+                bw.write(1+"\n");
+            }else {
+                bw.write(0+"\n");
+            }
         }else {
-            System.out.println(0);
+            bw.write(0+"\n");
         }
-     }
+        br.close();
+        bw.close();
+    }
 }
